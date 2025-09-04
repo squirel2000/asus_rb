ae388b3e906b18d129b0625ad3a4c45d2f1a9b87
Ming Li <ming_li@asus.com>
2025-09-04 10:30:08 +0800

    Fixed compatibility with ROS2 Jazzy.

--- src/slamware_ros_sdk/src/server/msg_convert.h
+++ src/slamware_ros_sdk/src/server/msg_convert.h
@@ -6,7 +6,7 @@
 #include "tf2/LinearMath/Matrix3x3.h"
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/impl/utils.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <sensor_msgs/msg/imu.hpp>
 
 #include <slamware_ros_sdk/utils.h>
